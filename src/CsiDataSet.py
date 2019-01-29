f8c33d04a0e053a0a6b1cfbcd8131638f2e44cf7
spacegoing <spacegoing@gmail.com>
2019-01-29 19:15:23 +1100

    [New] multi-task dataset

--- src/CsiDataSet.py
+++ src/CsiDataSet.py
@@ -8,6 +8,9 @@ from LagShiftUtils import DateRolling
 
 
 class Trainset(Dataset):
+  '''
+  Only for single task, csi300 with or without indicator usage
+  '''
 
   def __init__(self, csi_df, opt):
     super().__init__()
@@ -19,10 +22,15 @@ class Trainset(Dataset):
   def __getitem__(self, idx):
     sample = self.roller.get_sample_by_index(idx)
     return {
-        'X': sample['X'].loc[:, self.opt.x_columns].values,
-        'Y': sample['X'].loc[:, self.opt.y_columns].values,
-        'Y_gt': sample['Y'].loc[self.opt.y_columns].item(),
-        'idx': idx
+        'X':
+            sample.loc[:, sample.columns.difference(self.opt.y_columns)]
+            .values[:self.opt.lag_steps],
+        'Y':
+            sample.loc[:, self.opt.y_columns].values[:self.opt.lag_steps],
+        'Y_gt':
+            sample.loc[:, self.opt.y_columns].iloc[-1].item(),
+        'idx':
+            idx
     }
 
   def __len__(self):
@@ -30,6 +38,44 @@ class Trainset(Dataset):
 
   def get_idx_df(self, idx):
     return self.roller.get_sample_by_index(idx)
+
+
+class CSI300Dataset:
+
+  def get_dataset_loader(self, opt):
+    proced_csi_dir = [
+        '/project/chli/dataset_csi300/train_norm/',
+        '/project/chli/dataset_csi300/valid_norm/',
+        '/project/chli/dataset_csi300/test_norm/'
+    ]
+    train_df, valid_df, test_df = [
+        pd.read_csv(
+            i + 'csi300_ta_norm.csv',
+            nrows=1e6 if opt.debug else None,
+            parse_dates=['datetime'],
+            index_col=['con_code', 'datetime']) for i in proced_csi_dir
+    ]
+
+    # timesteps = 9  # t, t-1, ... t-8
+    # pred_timesteps = 1
+    # batchsize = 128
+    batchsize = opt.batchsize
+    shuffle = opt.shuffle
+    num_workers = opt.num_workers
+    pin_memory = opt.pin_memory
+
+    train_dataset, valid_dataset, test_dataset = Trainset(
+        train_df, opt), Trainset(valid_df, opt), Trainset(test_df, opt)
+    train_loader, valid_loader, test_loader = [
+        DataLoader(
+            i,
+            batch_size=batchsize,
+            shuffle=shuffle,
+            num_workers=num_workers,
+            pin_memory=pin_memory)
+        for i in [train_dataset, valid_dataset, test_dataset]
+    ]
+    return train_dataset, valid_dataset, test_dataset, train_loader, valid_loader, test_loader
 
 
 class MultiTaskTrainset(Dataset):
@@ -47,8 +93,28 @@ class MultiTaskTrainset(Dataset):
     self.opt = opt
 
   def __getitem__(self, idx):
-    sample_df = self.roller.get_std_sample_by_index(idx)
-    return {'sample_df': sample_df, 'idx': idx}
+    sample = self.roller.get_std_sample_by_index(idx)
+    return {
+        'X_trend':
+            sample
+            .loc[:, sample.columns.difference(['c', 'std', 'multi_target'])]
+            .values[:self.opt.lag_steps],
+        'Y_trend':
+            sample.loc[:, 'c'].values[:self.opt.lag_steps],
+        'Y_gt_trend':
+            sample.loc[:, 'c'].iloc[-1].item(),
+        'X_volat':
+            sample.loc[:, sample.columns.difference(['std', 'multi_target'])]
+            .values[:self.opt.lag_steps],
+        'Y_volat':
+            sample.loc[:, 'std'].values[:self.opt.lag_steps],
+        'Y_gt_volat':
+            sample.loc[:, 'std'].iloc[-1].item(),
+        'Y_gt_multi':
+            sample.loc[:, 'multi_target'].iloc[-1].item(),
+        'idx':
+            idx
+    }
 
   def __len__(self):
     return self.roller.num_samples
@@ -57,16 +123,22 @@ class MultiTaskTrainset(Dataset):
     return self.roller.get_std_sample_by_index(idx)
 
 
-class CSI300Dataset:
-
-  def __init__(self, opt):
-    self.csi_df = pd.read_csv(
-        opt.norm_csi_dir + 'csi300norm.csv',
-        nrows=1e6 if opt.debug else None,
-        parse_dates=['datetime'],
-        index_col=['con_code', 'datetime'])
+class CSI300MultiTaskDataset:
 
   def get_dataset_loader(self, opt):
+    proced_csi_dir = [
+        '/project/chli/dataset_csi300/train_norm/',
+        '/project/chli/dataset_csi300/valid_norm/',
+        '/project/chli/dataset_csi300/test_norm/'
+    ]
+    train_df, valid_df, test_df = [
+        pd.read_csv(
+            i + 'csi300_norm.csv',
+            nrows=1e6 if opt.debug else None,
+            parse_dates=['datetime'],
+            index_col=['con_code', 'datetime']) for i in proced_csi_dir
+    ]
+
     # timesteps = 9  # t, t-1, ... t-8
     # pred_timesteps = 1
     # batchsize = 128
@@ -74,22 +146,10 @@ class CSI300Dataset:
     shuffle = opt.shuffle
     num_workers = opt.num_workers
     pin_memory = opt.pin_memory
-    dataset_split_ratio = opt.dataset_split_ratio
 
-    # split train valid test dataset by number of stocks
-    split_index_ser = self.csi_df.iloc[:, 0].groupby(
-        level='con_code').count().cumsum()
-    stocks_num = split_index_ser.shape[0]
-    train_stocks_num = int(stocks_num * dataset_split_ratio[0])
-    train_index = split_index_ser.iloc[train_stocks_num - 1]
-    valid_stocks_num = int(stocks_num * sum(dataset_split_ratio[:2]))
-    valid_index = split_index_ser.iloc[valid_stocks_num - 1]
-    train_df = self.csi_df.iloc[:train_index]
-    valid_df = self.csi_df.iloc[train_index:valid_index]
-    test_df = self.csi_df.iloc[valid_index:]
-
-    train_dataset, valid_dataset, test_dataset = Trainset(
-        train_df, opt), Trainset(valid_df, opt), Trainset(test_df, opt)
+    train_dataset, valid_dataset, test_dataset = MultiTaskTrainset(
+        train_df, opt), MultiTaskTrainset(valid_df, opt), MultiTaskTrainset(
+            test_df, opt)
     train_loader, valid_loader, test_loader = [
         DataLoader(
             i,
@@ -105,21 +165,31 @@ class CSI300Dataset:
 if __name__ == "__main__":
   from main import opt
 
-  opt.debug = True
-  csi300 = CSI300Dataset(opt)
-  # for debug
-  # csi_df = pd.read_csv(
-  #     opt.norm_csi_dir + 'csi300norm.csv',
-  #     nrows=1e6 if opt.debug else None,
-  #     parse_dates=['datetime'],
-  #     index_col=['con_code', 'datetime'])
-  # csi300 = CSI300Dataset(csi_df, opt)
+  opt.debug = False
+  opt.ind_steps = 10
+  # single task ind_steps must be 0
+  opt.task_type = 'multi'  # 'multi' 'single'
+  opt.pred_type = 'shift'  # 'shift' 'steps'
 
+  csi300 = CSI300Dataset()
   train_dataset, valid_dataset, test_dataset, train_loader, valid_loader, test_loader = csi300.get_dataset_loader(
       opt)
   for i, d in enumerate(train_loader):
     if i % 100 == 0 and i != 0:
       break
+  for k, v in d.items():
+    print(k)
+    print(v.shape)
+
+  multi_csi300 = CSI300MultiTaskDataset()
+  mtrain_dataset, mvalid_dataset, mtest_dataset, mtrain_loader, mvalid_loader, mtest_loader = multi_csi300.get_dataset_loader(
+      opt)
+  for i, d in enumerate(mtrain_loader):
+    if i % 100 == 0 and i != 0:
+      break
+  for k, v in d.items():
+    print(k)
+    print(v.shape)
 
   # # double check
   # i=-1
